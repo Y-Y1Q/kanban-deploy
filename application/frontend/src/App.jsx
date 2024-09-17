03237748a079f2664147e833e28034adeabbb238
Y-Y1Q <sh4bee@outlook.com>
2024-09-16 21:10:59 -0700

    Config react url route

--- application/frontend/src/App.jsx
+++ application/frontend/src/App.jsx
@@ -1,30 +1,30 @@
 import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 import AboutUs from "./pages/About/AboutUs";
-// import AboutRishita from "./pages/About/Rishita";
-// import AboutLuai from "./pages/About/Luai";
-// import AboutL from "./pages/About/L";
-// import AboutSalah from "./pages/About/Salah";
-// import AboutZaw from "./pages/About/Zaw";
+import AboutRishita from "./pages/About/Rishita";
+import AboutLuai from "./pages/About/Luai";
+import AboutL from "./pages/About/L";
+import AboutSalah from "./pages/About/Salah";
+import AboutZaw from "./pages/About/Zaw";
 import AboutYee from "./pages/About/Yee";
 
 function App() {
 
   return (
     <BrowserRouter>
-        <Routes>
-          {/* Route for the team section */}
-          <Route path="/" element={<AboutUs />} />
+      <Routes>
+        {/* Route for the team section */}
+        <Route path="/" element={<AboutUs />} />
 
-          {/* Routes for each team member's profile */}
+        {/* Routes for each team member's profile */}
 
-          {/* <Route path="/about/rishita" element={<AboutRishita />} />
-          <Route path="/about/luai" element={<AboutLuai />} />
-          <Route path="/about/l" element={<AboutL />} />
-          <Route path="/about/salah" element={<AboutSalah />} />
-          <Route path="/about/zaw" element={<AboutZaw />} /> */}
-          <Route path="/about/yee" element={<AboutYee />} />
-        </Routes>
+        <Route path="/about/rishita" element={<AboutRishita />} />
+        <Route path="/about/luai" element={<AboutLuai />} />
+        <Route path="/about/l" element={<AboutL />} />
+        <Route path="/about/salah" element={<AboutSalah />} />
+        <Route path="/about/zaw" element={<AboutZaw />} />
+        <Route path="/about/yee" element={<AboutYee />} />
+      </Routes>
     </BrowserRouter>
 
 
