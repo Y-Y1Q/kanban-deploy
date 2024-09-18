982d24e65507b22566155dbcc31110841ab0b8c0
Tsu <112430082+lchowGH@users.noreply.github.com>
2024-09-17 22:56:15 -0700

    L: Updated About Me

--- application/frontend/src/pages/About/L/index.jsx
+++ application/frontend/src/pages/About/L/index.jsx
@@ -12,27 +12,33 @@ export default function AboutMe() {
                  or upload your own image
                 */}
                 <img
-                    src="https://api.dicebear.com/9.x/croodles-neutral/svg?seed=Christian"
-                    alt="Profile"
+                    src="https://api.dicebear.com/9.x/icons/svg?seed=Chase"
+                    alt="avatar"
                     className="pic"
                 />
 
 
                 {/* Edit description below */}
                 <div className="text">
-                    <h2>About Me</h2>
-                    <h5>Lorem ipsum dolor & <span>Lorem ipsum dolor</span></h5>
+                    <h2>L Chow</h2>
+
+                    <h3></h3>
+
                     <p>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita
-                        natus ad sed harum itaque ullam enim quas, veniam accusantium, quia
-                        animi id eos adipisci iusto molestias asperiores explicabo cum vero
-                        atque amet corporis! Soluta illum facere consequuntur magni. Ullam
-                        dolorem repudiandae cumque voluptate consequatur consectetur, eos
-                        provident necessitatibus reiciendis corrupti!
+                        Hello, I'm a Senior who is on their last semester here at SFSU! 
+                    </p>
+                        
+                    <p>
+                        I'm trying to become a Software Developer, preferably working in either Java or Python, 
+                        but also trying to explore other types of jobs as well.
                     </p>
 
+                    <p>
+                        On my free time, I enjoy watching anime, listening to music and going on hikes/walks.
+                    </p>
+                
                     <div className="data">
-                        <a href="https://github.com" className="link">
+                        <a href="https://github.com/lchowGH" className="link">
                             GitHub
                         </a>
                     </div>
