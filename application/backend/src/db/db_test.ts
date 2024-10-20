ce0da8c9d4abe74017721f45926300ba59e28461
Y-Y1Q <sh4bee@outlook.com>
2024-10-20 14:25:21 -0700

    Add instruction for query test

--- application/backend/src/db/db_test.ts
+++ application/backend/src/db/db_test.ts
@@ -1,5 +1,12 @@
 // General test function to test query
 // use tsx to run the query file you want to test
+/*
+Usage:
+
+import { testQuery } from "../db_test";
+
+testQuery(queryFuncToTest, paramsToTest)
+*/
 export async function testQuery(queryFunction: (...args: any[]) => Promise<any>, ...params: any[]) {
   try {
     console.log(`Running query with params: ${JSON.stringify(params)}`);
