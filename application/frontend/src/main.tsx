8b082df1f95f0ef14ea8fa45ab0e5a47f87d82ee
Y-Y1Q <sh4bee@outlook.com>
2024-10-20 17:34:43 -0700

    Update frontend package

--- application/frontend/src/main.tsx
+++ application/frontend/src/main.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import * as dotenv from "dotenv"
+import axios from "axios";
 
 import App from "./App.js";
+
+dotenv.config();
+axios.defaults.baseURL= process.env.BASE_URL;
+axios.defaults.withCredentials = true;
+
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
