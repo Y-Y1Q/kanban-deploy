ce99de0008bce0c7827e9444cbfc3aaed348d89f
Y-Y1Q <sh4bee@outlook.com>
2024-10-20 20:18:12 -0700

    Update frontend settings

--- application/frontend/src/App.tsx
+++ application/frontend/src/App.tsx
@@ -1,5 +1,7 @@
-import React from "react";
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+// import React from "react";
+import { MantineProvider } from "@mantine/core";
+import "@mantine/core/styles.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import AboutUs from "./pages/About/AboutUs";
 import AboutL from "./pages/About/L";
@@ -8,23 +10,23 @@ import AboutRishita from "./pages/About/Rishita";
 import AboutYee from "./pages/About/Yee";
 import AboutZaw from "./pages/About/Zaw";
 
-function App() {
+export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        {/* Route for the team section */}
-        <Route path="/" element={<AboutUs />} />
+    <MantineProvider>
+      <BrowserRouter>
+        <Routes>
+          {/* Route for the team section */}
+          <Route path="/about-us" element={<AboutUs />} />
 
-        {/* Routes for each team member's profile */}
+          {/* Routes for each team member's profile */}
 
-        <Route path="/about/rishita" element={<AboutRishita />} />
-        <Route path="/about/luai" element={<AboutLuai />} />
-        <Route path="/about/l" element={<AboutL />} />
-        <Route path="/about/zaw" element={<AboutZaw />} />
-        <Route path="/about/yee" element={<AboutYee />} />
-      </Routes>
-    </BrowserRouter>
+          <Route path="/about/rishita" element={<AboutRishita />} />
+          <Route path="/about/luai" element={<AboutLuai />} />
+          <Route path="/about/l" element={<AboutL />} />
+          <Route path="/about/zaw" element={<AboutZaw />} />
+          <Route path="/about/yee" element={<AboutYee />} />
+        </Routes>
+      </BrowserRouter>
+    </MantineProvider>
   );
 }
-
-export default App;
