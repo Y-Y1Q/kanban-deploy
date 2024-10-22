b4e63e64b9879868267957fd60af7086d9475b67
Y-Y1Q <sh4bee@outlook.com>
2024-10-22 03:53:20 -0700

    Update frontend base url

--- application/frontend/vite.config.ts
+++ application/frontend/vite.config.ts
@@ -1,22 +1,7 @@
 import react from "@vitejs/plugin-react-swc";
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  // Load environment variables based on the current mode (e.g., 'development', 'production')
-  const env = loadEnv(mode, process.cwd(), "");
-
-  return {
-    plugins: [react()], // Add React plugin
-    server: {
-      proxy: {
-        // Use the environment variable for the proxy target
-        "/api": {
-          target: env.VITE_API_URL, // Use the VITE_API_URL from the .env file
-          changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/api/, ""),
-        },
-      },
-    },
-  };
+export default defineConfig({
+  plugins: [react()],
 });
