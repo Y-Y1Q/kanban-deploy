b197f178f8809ccef539144a6e61f5e748aa5f2b
Y-Y1Q <sh4bee@outlook.com>
2024-10-22 18:12:29 -0700

    Update vite to use path rewrite

--- application/backend/src/middleware/request_time.ts
+++ application/backend/src/middleware/request_time.ts
@@ -5,7 +5,9 @@ export function requestTime(req: Request, _res: Response, next: NextFunction) {
   if (!req.url.startsWith("/dist") && req.url !== "/favicon.ico") {
     const timestamp = new Date().toLocaleString();
     console.log(
-      `\nRequest \x1b[32m\x1b[1m ${req.method} ${req.url} \x1b[0m` + `received at ${timestamp}`
+      `\nRequest \x1b[32m\x1b[1m ${req.method} ${req.url} \x1b[0m` +
+        `received at ${timestamp}` +
+        `\nOrigin: \x1b[32m\x1b[1m ${req.protocol}://${req.get("host")}${req.originalUrl}\x1b[0m`
     );
   }
   next();
