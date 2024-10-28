708fdb4e807ea144c63596d49d7a310abab5cade
Y-Y1Q <sh4bee@outlook.com>
2024-10-27 19:35:14 -0700

    Add bar chart

--- application/frontend/src/pages/job_stats/Bar.tsx
+++ application/frontend/src/pages/job_stats/Bar.tsx
@@ -1,6 +1,7 @@
 import { Box } from "@mui/material";
 
 import PageIntro from "../../components/ui/PageIntro";
+import BarChart from "./components/BarChart";
 
 // import BarChart from "./components/BarChart";
 
@@ -8,7 +9,9 @@ export default function Bar() {
   return (
     <Box m="20px">
       <PageIntro title="Bar Chart" />
-      <Box height="75vh"></Box>
+      <Box height="75vh">
+        <BarChart />
+      </Box>
     </Box>
   );
 }
