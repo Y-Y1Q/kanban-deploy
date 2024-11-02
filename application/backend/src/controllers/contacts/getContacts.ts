befa4f32f92dd0b6a356e75093578c47335ac8e1
Tsu <112430082+lchowGH@users.noreply.github.com>
2024-11-01 19:23:11 -0700

    getContacts controller updated to working status. Tested all backend APIs for contacts using Thunder Client and are all working

--- application/backend/src/controllers/contacts/getContacts.ts
+++ application/backend/src/controllers/contacts/getContacts.ts
@@ -2,16 +2,19 @@ import { Request, Response } from "express";
 import { getContacts } from "../../db/contacts";
 
 export const getContact = async (req: Request, res: Response): Promise<void> => {
-  const { id } = req.params;
+  const { searchParam } = req.query as { searchParam: string };
+  console.log(`Received searchParam: ${searchParam}`);
   try {
-    const contact = await getContacts(id);
-    if (contact) {
-      res.status(200).json(contact);
+    const contacts = await getContacts(searchParam || "");
+    console.log(`Fetched contacts: ${JSON.stringify(contacts)}`);
+    if (contacts && contacts.length > 0) {
+      res.status(200).json(contacts);
     } else {
-      res.status(404).send(`Contact not found.`);
+      res.status(404).send(`Contacts not found.`);
     }
   } catch (error) {
     const errorMessage = error instanceof Error ? error.message : "Unknown error";
-    res.status(500).send(`Error getting contact: ${errorMessage}`);
+    console.error(`Error getting contacts: ${errorMessage}`);
+    res.status(500).send(`Error getting contacts: ${errorMessage}`);
   }
 };
